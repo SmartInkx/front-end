ef5a0d816664ab092cc4fc127d484705caf3f180
Tauane Lima <Tauanesantos10@hotmail.com>
2022-04-03 22:03:09 -0300

    modal

--- src/app/engrenagem/engrenagem.component.ts
+++ src/app/engrenagem/engrenagem.component.ts
@@ -1,5 +1,6 @@
 import { EngrenagemService } from './engrenagem.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-engrenagem',
@@ -19,6 +20,17 @@ export class EngrenagemComponent implements OnInit {
   listar(){
     this.engrenagemService.listar().subscribe(dados => this.login = dados);
   }
-
-
 }
+
+export class Dialog {
+  constructor(public dialog: MatDialog){}
+  openDialog() {
+    const dialogRef = this.dialog.open(Dialogo);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+export class Dialogo{}
+
